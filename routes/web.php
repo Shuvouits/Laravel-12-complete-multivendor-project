a4bba240ce054915c5a34ce997fe2b05f3919734
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2026-04-09 15:19:00 +0600

    UserDashboard frotnend mastering

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\frontend\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\user\UserDashboardController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -13,9 +14,12 @@ Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 //User
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::group(['middleware' => ['auth', 'verified']], function () {
+     Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+
+});
+
+
 
 
 

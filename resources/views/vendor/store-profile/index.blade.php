54e96974fc0384b8118d3a63a50a7628c6d9906f
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2026-05-02 13:06:39 +0600

    Role & Permission Continue

--- resources/views/vendor/store-profile/index.blade.php
+++ resources/views/vendor/store-profile/index.blade.php
@@ -4,12 +4,12 @@
 
 @section('content')
     <div class="page-body">
-        <div class="container-xl">
+        <div class="container-fluid px-4">
             <div class="row justify-content-center">
-                <div class="col-lg-10">
+                <div class="col-12 col-xxl-11">
                     <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Create Store</h3>
+                        <div class="card-header py-3">
+                            <h3 class="card-title mb-0">Create Store</h3>
                         </div>
 
                         <form action="{{ route('vendor.store-profile.update', 1) }}" method="POST"
@@ -17,8 +17,7 @@
                             @csrf
                             @method('PUT')
 
-
-                            <div class="card-body">
+                            <div class="card-body px-4 px-md-5 py-4">
 
                                 @if ($errors->any())
                                     <div class="alert alert-danger">
@@ -30,11 +29,12 @@
                                     </div>
                                 @endif
 
-                                <div class="row">
-                                    <div class="col-md-6 mb-4">
+                                <div class="row g-4">
+                                    <div class="col-12 col-xl-6">
                                         <label class="form-label">Store Logo <span class="text-danger">*</span></label>
                                         <input type="file" name="logo" id="logoInput"
-                                            class="form-control @error('logo') is-invalid @enderror" accept="image/*">
+                                            class="form-control form-control-lg @error('logo') is-invalid @enderror"
+                                            accept="image/*">
 
                                         @error('logo')
                                             <div class="invalid-feedback">{{ $message }}</div>
@@ -43,14 +43,15 @@
                                         <div class="mt-3">
                                             <img id="logoPreview" src="https://placehold.co/150x150?text=Logo+Preview"
                                                 alt="Logo Preview" class="img-fluid rounded border"
-                                                style="width: 150px; height: 150px; object-fit: cover;">
+                                                style="width: 180px; height: 180px; object-fit: cover;">
                                         </div>
                                     </div>
 
-                                    <div class="col-md-6 mb-4">
+                                    <div class="col-12 col-xl-6">
                                         <label class="form-label">Store Banner <span class="text-danger">*</span></label>
                                         <input type="file" name="banner" id="bannerInput"
-                                            class="form-control @error('banner') is-invalid @enderror" accept="image/*">
+                                            class="form-control form-control-lg @error('banner') is-invalid @enderror"
+                                            accept="image/*">
 
                                         @error('banner')
                                             <div class="invalid-feedback">{{ $message }}</div>
@@ -59,16 +60,16 @@
                                         <div class="mt-3">
                                             <img id="bannerPreview" src="https://placehold.co/600x200?text=Banner+Preview"
                                                 alt="Banner Preview" class="img-fluid rounded border"
-                                                style="width: 100%; height: 200px; object-fit: cover;">
+                                                style="width: 100%; height: 220px; object-fit: cover;">
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="row">
-                                    <div class="col-md-6 mb-3">
+                                <div class="row g-4 mt-1">
+                                    <div class="col-12 col-xl-6">
                                         <label class="form-label">Store Name <span class="text-danger">*</span></label>
                                         <input type="text" name="name" value="{{ old('name') }}"
-                                            class="form-control @error('name') is-invalid @enderror"
+                                            class="form-control form-control-lg @error('name') is-invalid @enderror"
                                             placeholder="Enter store name">
 
                                         @error('name')
@@ -76,10 +77,10 @@
                                         @enderror
                                     </div>
 
-                                    <div class="col-md-6 mb-3">
+                                    <div class="col-12 col-xl-6">
                                         <label class="form-label">Phone</label>
                                         <input type="text" name="phone" value="{{ old('phone') }}"
-                                            class="form-control @error('phone') is-invalid @enderror"
+                                            class="form-control form-control-lg @error('phone') is-invalid @enderror"
                                             placeholder="Enter phone number">
 
                                         @error('phone')
@@ -87,10 +88,10 @@
                                         @enderror
                                     </div>
 
-                                    <div class="col-md-6 mb-3">
+                                    <div class="col-12 col-xl-6">
                                         <label class="form-label">Email</label>
                                         <input type="email" name="email" value="{{ old('email') }}"
-                                            class="form-control @error('email') is-invalid @enderror"
+                                            class="form-control form-control-lg @error('email') is-invalid @enderror"
                                             placeholder="Enter email address">
 
                                         @error('email')
@@ -98,10 +99,10 @@
                                         @enderror
                                     </div>
 
-                                    <div class="col-md-6 mb-3">
+                                    <div class="col-12 col-xl-6">
                                         <label class="form-label">Address</label>
                                         <input type="text" name="address" value="{{ old('address') }}"
-                                            class="form-control @error('address') is-invalid @enderror"
+                                            class="form-control form-control-lg @error('address') is-invalid @enderror"
                                             placeholder="Enter address">
 
                                         @error('address')
@@ -109,11 +110,11 @@
                                         @enderror
                                     </div>
 
-                                    <div class="col-12 mb-3">
+                                    <div class="col-12">
                                         <label class="form-label">Short Description</label>
                                         <input type="text" name="short_description"
                                             value="{{ old('short_description') }}"
-                                            class="form-control @error('short_description') is-invalid @enderror"
+                                            class="form-control form-control-lg @error('short_description') is-invalid @enderror"
                                             placeholder="Enter short description">
 
                                         @error('short_description')
@@ -121,10 +122,10 @@
                                         @enderror
                                     </div>
 
-                                    <div class="col-12 mb-3">
+                                    <div class="col-12">
                                         <label class="form-label">Long Description</label>
 
-                                        <div id="editor" style="height: 300px;">
+                                        <div id="editor" style="height: 320px;">
                                             {!! old('long_description') !!}
                                         </div>
 
@@ -137,9 +138,9 @@
                                 </div>
                             </div>
 
-                            <div class="card-footer text-end">
-                                <a href="{{ url()->previous() }}" class="btn btn-outline-secondary">Cancel</a>
-                                <button type="submit" class="btn btn-primary">Save Store</button>
+                            <div class="card-footer text-end py-3 px-4 px-md-5">
+                                <a href="{{ url()->previous() }}" class="btn btn-outline-secondary btn-lg">Cancel</a>
+                                <button type="submit" class="btn btn-outline-primary btn-lg">Save Store</button>
                             </div>
                         </form>
                     </div>
@@ -147,8 +148,6 @@
             </div>
         </div>
     </div>
-
-
 @endsection
 
 @push('scripts')
